2f79cbcf0357536ebeef092cd68957e0bcb890cd
SeborExe <mix98@o2.pl>
2022-05-24 19:34:22 +0200

    Damage
    Damage colliders has been added into weapons. Now player is able to deal damage and kill! Also basic enemy has been created.

---
+++ Soulslike/Assets/__Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+
+    public bool isDead;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        //playerManager = GetComponent<PlayerManager>();
+        anim = GetComponentInChildren<Animator>();
+
+        //staminaBar = FindObjectOfType<StaminaBar>();
+        //manaBar = FindObjectOfType<ManaBar>();
+    }
+
+    private void Start()
+    {
+        maxHealth = SetMaxLevelHalth();
+        currentHealth = maxHealth;
+    }
+
+    private int SetMaxLevelHalth()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //if (playerManager.isInvulnerable) return;
+        if (isDead) return;
+
+        currentHealth -= damage;
+
+        anim.Play("Damage_01");
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            anim.Play("Dead_01");
+            isDead = true;
+        }
+    }
+}
--- Soulslike/Assets/__Scripts/Player/WeaponSlotManager.cs
+++ Soulslike/Assets/__Scripts/Player/WeaponSlotManager.cs
@@ -7,10 +7,21 @@ public class WeaponSlotManager : MonoBehaviour
     WeaponHolderSlot leftHandSlot;
     WeaponHolderSlot rightHandSlot;
     PlayerManager playerManager;
+    PlayerStats playerStats;
+    InputHandler inputHandler;
+    PlayerInventory playerInventory;
+
+    public DamageCollider leftHandDamageCollider;
+    public DamageCollider rightHandDamageCollider;
 
     private void Awake()
     {
         playerManager = GetComponentInParent<PlayerManager>();
+        playerInventory = GetComponentInParent<PlayerInventory>();
+        //animator = GetComponent<Animator>();
+        //quickSlotsUI = FindObjectOfType<QuickSlotsUI>();
+        playerStats = GetComponentInParent<PlayerStats>();
+        inputHandler = GetComponentInParent<InputHandler>();
 
         WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
         foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
@@ -35,11 +46,52 @@ public class WeaponSlotManager : MonoBehaviour
         if (isLeft)
         {
             leftHandSlot.LoadWeaponModel(weaponItem);
+            LoadLeftWeaponCollider();
         }
         else
         {
             rightHandSlot.LoadWeaponModel(weaponItem);
+            LoadRightWeaponCollider();
         }
 
     }
+
+    #region Open and Close weapon collider
+
+    private void LoadLeftWeaponCollider()
+    {
+        leftHandDamageCollider = leftHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        leftHandDamageCollider.currentWeaponDamage = playerInventory.leftWeapon.baseDamage;
+    }
+
+    private void LoadRightWeaponCollider()
+    {
+        rightHandDamageCollider = rightHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        rightHandDamageCollider.currentWeaponDamage = playerInventory.rightWeapon.baseDamage;
+    }
+
+    public void OpenDamageCollider()
+    {
+        //if (playerManager.isUsingRightHand && rightHandDamageCollider != null)
+        if (rightHandDamageCollider != null)
+        {
+            rightHandDamageCollider.EnabelDamageCollider();
+        }
+        //else if (playerManager.isUsingLeftHand && leftHandDamageCollider != null)
+        else if (leftHandDamageCollider != null)
+        {
+            leftHandDamageCollider.EnabelDamageCollider();
+        }
+    }
+
+    public void CloseDamageCollider()
+    {
+        if (rightHandDamageCollider != null)
+            rightHandDamageCollider.DisableDamageCollider();
+
+        if (leftHandDamageCollider != null)
+            leftHandDamageCollider.DisableDamageCollider();
+    }
+
+    #endregion
 }
